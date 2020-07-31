bcdd5590e58c1f8542e7b432549baf71919f3dee
danielhuang-030 <danielhuang030@gmail.com>
2020-07-31 19:23:50 +0800

    refactor: user login

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,19 +4,27 @@ namespace App\Services;
 
 use App\Models\User;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
     /**
-     * UserRepository
+     * token key.
+     *
+     * @var string
+     */
+    const TOKEN_KEY = 'user';
+
+    /**
+     * UserRepository.
      *
      * @var UserRepository
      */
     protected $userRepository;
 
     /**
-     * construct
+     * construct.
      *
      * @param UserRepository $userRepository
      */
@@ -26,9 +34,10 @@ class UserService
     }
 
     /**
-     * create
+     * create.
      *
      * @param array $data
+     *
      * @return User
      */
     public function create(array $data)
@@ -36,5 +45,34 @@ class UserService
         $data['password'] = Hash::make($data['password']);
 
         return $this->userRepository->create($data);
+    }
+
+    /**
+     * attempt.
+     *
+     * @param array $credentials
+     *
+     * @return User
+     */
+    public function attempt(array $credentials)
+    {
+        $user = $this->userRepository->getByEmail($credentials['email']);
+        if (null === $user) {
+            return null;
+        }
+
+        if (!Hash::check($credentials['password'], $user->password)) {
+            return null;
+        }
+
+        // set token
+        $tokenResult = $user->createToken(static::TOKEN_KEY);
+        $tokenResult->token->save();
+        $user->withAccessToken($tokenResult->accessToken);
+
+        // set user
+        Auth::setUser($user);
+
+        return $user;
     }
 }
