b26409c9b0ed0abca1203db7a198d742c7b1a7ac
danielhuang-030 <danielhuang030@gmail.com>
2020-08-01 12:32:59 +0800

    chore: Authenticate render json

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,18 +3,44 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
     /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param array                    $guards
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+        try {
+            parent::authenticate($request, $guards);
+        } catch (\Throwable $th) {
+            if ($request->expectsJson()) {
+                throw new HttpResponseException(response()->json(['message' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED));
+            }
+
+            throw $th;
+        }
+    }
+
+    /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return string
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
             return route('login');
         }
     }
