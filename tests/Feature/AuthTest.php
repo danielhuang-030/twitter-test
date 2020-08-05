e55bab1fda2b331c4984897804171afb059e9e0a
danielhuang-030 <danielhuang030@gmail.com>
2020-08-05 14:28:28 +0800

    refactor: auth test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Hash;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
@@ -9,16 +11,32 @@ class AuthTest extends TestCase
 {
     use WithFaker;
 
-    private $password = 'mypassword';
+    private $name;
+    private $email;
+    private $password;
 
-    public function testAuthSignup()
+    /**
+     * set up.
+     */
+    protected function setUp(): void
     {
-        $name = $this->faker->name();
-        $email = $this->faker->email();
+        parent::setUp();
 
+        $this->name = $this->faker->name();
+        $this->email = $this->faker->email();
+        $this->password = 'password';
+    }
+
+    /**
+     * signup.
+     *
+     * @return void
+     */
+    public function testSignup()
+    {
         $response = $this->postJson('/api/signup', [
-            'name'                  => $name,
-            'email'                 => $email,
+            'name'                  => $this->name,
+            'email'                 => $this->email,
             'password'              => $this->password,
             'password_confirmation' => $this->password,
         ]);
@@ -26,6 +44,34 @@ class AuthTest extends TestCase
         $response->assertStatus(200)
             ->assertExactJson([
                 'message' => 'Successfully created user!',
+            ]);
+    }
+
+    /**
+     * login.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $user = factory(User::class)->create([
+            'email'    => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email'    => $this->email,
+            'password' => $this->password,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+                'token',
             ]);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,32 @@
 
 namespace Tests;
 
+use Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    /**
+     * set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('migrate', [
+            '-vvv' => true,
+        ]);
+        Artisan::call('passport:install', [
+            '-vvv' => true,
+        ]);
+        Artisan::call('db:seed', [
+            '-vvv' => true,
+        ]);
+    }
 }
