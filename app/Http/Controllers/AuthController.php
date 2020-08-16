0cf084ee793cb99835ce3d5c694efe542af94459
danielhuang-030 <danielhuang030@gmail.com>
2020-08-16 17:31:14 +0800

    chore: add openapi anotations

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,14 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthController extends Controller
 {
     /**
+     * @OA\SecurityScheme(
+     *     securityScheme="passport",
+     *     type="http",
+     *     scheme="bearer",
+     * )
+     */
+
+    /**
      * UserService.
      *
      * @var UserService
@@ -29,6 +37,103 @@ class AuthController extends Controller
 
     /**
      * signup.
+     *
+     * @OA\Post(
+     *     path="/api/signup",
+     *     summary="Signup",
+     *     description="Signup",
+     *     tags={"Auth"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 required={"name", "email", "password", "password_confirmation"},
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string",
+     *                     format="string",
+     *                     description="name",
+     *                     example="test001",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="string",
+     *                     description="email",
+     *                     example="test001@test.com",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     format="password",
+     *                     description="password",
+     *                     example="123456",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password_confirmation",
+     *                     type="string",
+     *                     format="password",
+     *                     description="password confirmation",
+     *                     example="123456",
+     *                 ),
+     *             ),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successfully.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="Successfully created user!",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Failed.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="Failed to create user!",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Validation error.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="The email field is required.",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     * )
      *
      * @param SignupRequest $request
      */
@@ -52,6 +157,88 @@ class AuthController extends Controller
     /**
      * login.
      *
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="Login",
+     *     description="Login",
+     *     tags={"Auth"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 required={"email", "password"},
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="string",
+     *                     description="email",
+     *                     example="test001@test.com",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     format="password",
+     *                     description="password",
+     *                     example="123456",
+     *                 ),
+     *             ),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successfully.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(property="id", type="integer" ,format="int64", example=1),
+     *                     @OA\Property(property="name", type="string", format="string", example="test001"),
+     *                     @OA\Property(property="email", type="string", format="string", example="test001@test.com"),
+     *                     @OA\Property(property="created_at", type="string", format="date-time", example="2020-07-31 23:54:28"),
+     *                     @OA\Property(property="updated_at", type="string", format="date-time", example="2020-07-31 23:54:28"),
+     *                     @OA\Property(property="token", type="string", format="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJ..."),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="Unauthorized",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Validation error.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="The email field is required.",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     * )
+     *
      * @param LoginRequest $request
      */
     public function login(LoginRequest $request)
@@ -73,6 +260,56 @@ class AuthController extends Controller
 
     /**
      * Logout user (Revoke the token).
+     *
+     * @OA\Get(
+     *     path="/api/logout",
+     *     summary="Logout",
+     *     description="Logout",
+     *     tags={"Auth"},
+     *     security={
+     *         {
+     *             "passport": {},
+     *         },
+     *     },
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successfully.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="Successfully logged out",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized.",
+     *         content={
+     *             @OA\MediaType(
+     *                 mediaType="application/json",
+     *                 @OA\Schema(
+     *                     @OA\Property(
+     *                         property="message",
+     *                         type="string",
+     *                         format="string",
+     *                         description="message",
+     *                         example="Unauthorized",
+     *                     ),
+     *                 ),
+     *             ),
+     *         },
+     *     ),
+     * )
+     *
+     * @param Request $request
      */
     public function logout(Request $request)
     {
