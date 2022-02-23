2695cbb6d29098122ef9d3cb6e59c54ff86cef99
Daniel <daniel.huang@coolbe.com.tw>
2022-02-23 17:23:13 +0800

    feat: add package.json, laravel-echo, socket.io-client

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,16 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+
+window.Echo.channel('new-user-following-uesr-2')
+    .listen('UserFollowCreated', (e) => {
+        console.log("UserFollowCreated");
+        console.log(e);
+    });
+
+window.Echo.channel('new-user-unfollow-uesr-2')
+    .listen('UserFollowDeleted', (e) => {
+        console.log("UserFollowDeleted");
+        console.log(e);
+    });
