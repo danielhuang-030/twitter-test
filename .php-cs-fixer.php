74d2e14e10fd9f5ad2881c425ee881b6f34875b4
Daniel <daniel.huang@coolbe.com.tw>
2023-01-10 18:00:19 +0800

    chore: .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,18 +2,18 @@
 
 return (new \PhpCsFixer\Config())
     ->setRules([
-        '@PSR1'                  => true,
-        '@PSR2'                  => true,
-        '@Symfony'               => true,
-        'ordered_class_elements' => true,
-        'ordered_imports'        => [
+        '@PSR1'                      => true,
+        '@PSR2'                      => true,
+        '@Symfony'                   => true,
+        'ordered_class_elements'     => true,
+        'ordered_imports'            => [
             'sort_algorithm' => 'alpha',
         ],
-        'binary_operator_spaces' => [
-            'operators' => [
-                '=>' => 'align_single_space_minimal',
-            ],
-        ],
+        // 'binary_operator_spaces' => [
+        //     'operators' => [
+        //         '=>' => 'align_single_space_minimal',
+        //     ],
+        // ],
 
         'phpdoc_no_empty_return'     => false,
         'phpdoc_no_package'          => false,
