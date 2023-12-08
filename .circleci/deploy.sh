a7173d79368dc213e0912fb6afb299098bdbfef8
Daniel <danielhuang030@gmail.com>
2023-12-08 23:51:55 +0800

    chore: circleci deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -31,4 +31,10 @@ $docker_command php artisan octane:stop
 # rise from the ashes
 $docker_command php artisan up
 
+# npm install
+$docker_command npm install
+
+# npm run build
+$docker_command npm run build
+
 echo 'Deploy finished.'
