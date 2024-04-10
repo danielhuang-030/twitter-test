2485f6a4ab152568431a88c5db40509f0e800fc0
Daniel <danielhuang@rakuya.com.tw>
2024-04-10 17:35:51 +0800

    chore: remove crawler commands for ruten

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,17 +18,12 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param Schedule $schedule
      *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('crawler:ruten_c')->everyMinute();
-        // $schedule->command('crawler:ruten_i')->everyMinute();
-        // $schedule->command('crawler:ruten_p')->everyMinute();
-        // $schedule->command('crawler:ruten_s')->everyMinute();
-        // $schedule->command('crawler:ruten_store')->everyMinute();
     }
 
     /**
