f7fbb26e25e90997e431eb1b6c49455aaa5d71da
Daniel <danielhuang@rakuya.com.tw>
2024-06-06 15:06:22 +0800

    refactor: use vite to package

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,11 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
+        commonjs()
     ],
+    // server: {
+    //     cors: true,
+    //     origin: 'http://172.20.15.125:13000', // 根據你的後端運行的地址調整
+    // },
 });
