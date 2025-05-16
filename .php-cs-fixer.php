3909331098a7462379b7b15ec6956d7015d87109
Daniel <danielhuang@rakuya.com.tw>
2025-05-16 13:59:31 +0800

    chore: .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,21 +1,15 @@
 <?php
 
-return (new \PhpCsFixer\Config())
+return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR1'                      => true,
-        '@PSR2'                      => true,
-        '@Symfony'                   => true,
-        'ordered_class_elements'     => true,
-        'ordered_imports'            => [
+        '@PSR1' => true,
+        '@PSR2' => true,
+        '@Symfony' => true,
+        'ordered_class_elements' => true,
+        'ordered_imports' => [
             'sort_algorithm' => 'alpha',
         ],
-        // 'binary_operator_spaces' => [
-        //     'operators' => [
-        //         '=>' => 'align_single_space_minimal',
-        //     ],
-        // ],
-
-        'phpdoc_no_empty_return'     => false,
-        'phpdoc_no_package'          => false,
+        'phpdoc_no_empty_return' => false,
+        'phpdoc_no_package' => false,
         'no_superfluous_phpdoc_tags' => false,
     ]);
